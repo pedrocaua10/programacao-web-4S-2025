a025a805b3477cf6804c6c8e5706be966f2fc308
PEDRO CAUÃ RODRIGUES PEREIRA <pedro.caua@a.ucb.br>
2025-03-06 11:25:04 -0300

    atividade2

---
+++ variaveis_tipos/variaveis_tipos.js
@@ -0,0 +1,18 @@
+const n = 3;
+
+var j = 6;
+var j = 7;
+
+let nome = 'Pedro';
+let numero = "1245"
+
+console.log("Meu nome e: " + nome + "Telefone" + telefone);
+console.log(`Meu nome e:' ${nome} Telefone: ${telefone}`);
+
+let x = 1;
+x = 0.5;
+x = 3.14;
+
+let b = true;
+b = false;
+
