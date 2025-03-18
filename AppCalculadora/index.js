8a1e66619a22f2a2b60dd4908bf76045b9b3c980
Pedro Cauã <142911674+pedrocaua10@users.noreply.github.com>
2025-03-18 12:35:05 -0300

    Atividade 3

--- AppCalculadora/index.js
+++ AppCalculadora/index.js
@@ -1,15 +1,43 @@
-const express = require('express')
-const calc = require('./calculadora')
-const app = express()
+const express = require("express");
+const calc = require("./calculadora");
+const app = express();
 
-app.get('/ola',(req, res)=>{
-    res.send('hello, world!');
+app.get("/ola", (req, res) => {
+  res.send("hello, world!");
 });
-app.get('/ola/:nome',(req, res)=>{
-    res.send('Ola, '+ req.params.nome);
+app.get("/ola/:nome", (req, res) => {
+  res.send("Ola, " + req.params.nome);
 });
-const PORT = 8080
 
-app.listen(PORT,()=>{
-    console.log('app rodando na porta'+ PORT)
+app.get("/somar/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const resultado = a + b;
+  res.send("Resultado da soma: " + resultado);
+});
+
+app.get("/subtrair/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const resultado = a - b;
+  res.send("Resultado da subtração: " + resultado);
+});
+
+app.get("/multiplicar/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const resultado = a * b;
+  res.send("Resultado da multiplicação: " + resultado);
+});
+
+app.get("/dividir/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const resultado = a / b;
+  res.send("Resultado da divisão: " + resultado);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
