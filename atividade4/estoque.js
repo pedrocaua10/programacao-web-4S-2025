fd128d3155928b9046d45ca727148efc0c9975d9
pedrocaua10 <pedro.caua4@gmail.com>
2025-03-27 10:14:42 -0300

    add atividade4

---
+++ atividade4/estoque.js
@@ -0,0 +1,20 @@
+
+function adicionar(id, nome, qtd){
+    
+}
+function listar (a, b){
+    return a - b;
+}
+function remover(a, b){
+    return a * b;
+}
+function editar(a, b){
+    return a / b;
+}
+
+module.exports = {
+    somar, 
+    subtrair,
+    multiplicar,
+    dividir
+}
