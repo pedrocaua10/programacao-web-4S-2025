7c126b5f6805cbe98ce64ad275242e7a51b02e48
Pedro Cauã <142911674+pedrocaua10@users.noreply.github.com>
2025-03-27 09:32:08 -0300

    Update index.js

--- AppCalculadora/index.js
+++ AppCalculadora/index.js
@@ -1,43 +1,36 @@
-const express = require("express");
-const calc = require("./calculadora");
+const express = require ('express');
+const calc = require('./calculadora');
 const app = express();
 
-app.get("/ola", (req, res) => {
-  res.send("hello, world!");
-});
-app.get("/ola/:nome", (req, res) => {
-  res.send("Ola, " + req.params.nome);
-});
-
-app.get("/somar/:a/:b", (req, res) => {
-  const a = Number(req.params.a);
-  const b = Number(req.params.b);
-  const resultado = a + b;
-  res.send("Resultado da soma: " + resultado);
+app.get('/operacao/somar/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a); 
+    const b = parseFloat(req.params.b); 
+    const resultado = calc.somar(a, b);  
+    res.send(`Resultado da soma: ${resultado}`);
 });
 
-app.get("/subtrair/:a/:b", (req, res) => {
-  const a = Number(req.params.a);
-  const b = Number(req.params.b);
-  const resultado = a - b;
-  res.send("Resultado da subtração: " + resultado);
+app.get('/operacao/subtrair/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a); 
+    const b = parseFloat(req.params.b); 
+    const resultado = calc.subtrair(a, b);  
+    res.send(`Resultado da subtracao: ${resultado}`);
 });
 
-app.get("/multiplicar/:a/:b", (req, res) => {
-  const a = Number(req.params.a);
-  const b = Number(req.params.b);
-  const resultado = a * b;
-  res.send("Resultado da multiplicação: " + resultado);
+app.get('/operacao/multiplicar/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a); 
+    const b = parseFloat(req.params.b); 
+    const resultado = calc.multiplicar(a, b);  
+    res.send(`Resultado da multiplicacao: ${resultado}`);
 });
 
-app.get("/dividir/:a/:b", (req, res) => {
-  const a = Number(req.params.a);
-  const b = Number(req.params.b);
-  const resultado = a / b;
-  res.send("Resultado da divisão: " + resultado);
+app.get('/operacao/dividir/:a/:b', (req, res) => {
+    const a = parseFloat(req.params.a); 
+    const b = parseFloat(req.params.b); 
+    const resultado = calc.dividir(a, b);  
+    res.send(`Resultado da divisao: ${resultado}`);
 });
 
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log('app rodando na porta ' +PORT);
 });
